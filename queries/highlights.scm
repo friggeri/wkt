ef95c7102bbc0d00525f12530e665d3fd5b67773
Adrien Friggeri <adrien@friggeri.net>
2024-04-09 12:57:36 +0000

    Added highlights

---
+++ queries/highlights.scm
@@ -0,0 +1,26 @@
+(number) @number
+(pace) @number
+
+[
+  (sec)
+  (min)
+  (hr)
+  (miles)
+  (yards)
+  (feet)
+  (meter)
+  (kilometer)
+  (z1)
+  (z2)
+  (z3)
+  (z4)
+  (z5)
+] @constant.builtin
+
+[
+  "+"
+  "x"
+  "("
+  ")"
+  "@"
+] @keyword
